0affe5a5329f90848fa11dad96bb78856b2d8203
Tracie <tracie@nwnerd.com>
2019-05-14 20:43:33 -0700

    Initial commit
    Saving logViewerExample found on Stackoverflow: https://stackoverflow.com/questions/16743804/implementing-a-log-viewer-with-wpf#

---
+++ LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogViewExample
+{
+    public class LogEntry : PropertyChangedBase
+    {
+        public DateTime DateTime { get; set; }
+
+        public int Index { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class CollapsibleLogEntry : LogEntry
+    {
+        public List<LogEntry> Contents { get; set; }
+    }
+}
